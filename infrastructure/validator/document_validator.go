6278a38db4d756d671191aba99ff33f29a3a5532
Gabriel <gabrielmoura@MacBook-Air-de-Gabriel-2.local>
2024-12-20 09:21:48 -0300

    fix: fixed import

--- infrastructure/validator/document_validator.go
+++ infrastructure/validator/document_validator.go
@@ -5,12 +5,15 @@ import (
 	"strconv"
 )
 
+// IsValidCPF validates a CPF number.
 func IsValidCPF(cpf string) bool {
 	re := regexp.MustCompile(`\D`)
 	cpf = re.ReplaceAllString(cpf, "")
+
 	if len(cpf) != 11 {
 		return false
 	}
+
 	invalids := []string{
 		"00000000000", "11111111111", "22222222222",
 		"33333333333", "44444444444", "55555555555",
@@ -21,10 +24,11 @@ func IsValidCPF(cpf string) bool {
 			return false
 		}
 	}
+
 	for i := 9; i < 11; i++ {
 		sum := 0
 		for j := 0; j < i; j++ {
-			num, _ := strconv.Atoi(string(cpf[j]))
+			num, _ := strconv.Atoi(string(cpf[j])) // Convert byte to string
 			sum += num * (i + 1 - j)
 		}
 		rest := sum % 11
@@ -33,19 +37,23 @@ func IsValidCPF(cpf string) bool {
 		} else {
 			rest = 11 - rest
 		}
-		if rest != atoi(cpf[i]) {
+		if rest != atoi(string(cpf[i])) { // Convert byte to string
 			return false
 		}
 	}
+
 	return true
 }
 
+// IsValidCNPJ validates a CNPJ number.
 func IsValidCNPJ(cnpj string) bool {
 	re := regexp.MustCompile(`\D`)
 	cnpj = re.ReplaceAllString(cnpj, "")
+
 	if len(cnpj) != 14 {
 		return false
 	}
+
 	invalids := []string{
 		"00000000000000", "11111111111111", "22222222222222",
 		"33333333333333", "44444444444444", "55555555555555",
@@ -56,12 +64,13 @@ func IsValidCNPJ(cnpj string) bool {
 			return false
 		}
 	}
+
 	var calcDigits = func(c string) bool {
 		length := len(c)
 		sum := 0
 		pos := length - 7
 		for i := length - 1; i >= 0; i-- {
-			num := atoi(string(c[length-1-i]))
+			num := atoi(string(c[length-1-i])) // Convert byte to string
 			sum += num * pos
 			pos--
 			if pos < 2 {
@@ -74,30 +83,36 @@ func IsValidCNPJ(cnpj string) bool {
 		} else {
 			result = 11 - result
 		}
-		return result == atoi(string(c[len(c)-1]))
+		return result == atoi(string(c[len(c)-1])) // Convert byte to string
 	}
+
 	// Check first digit
 	if !calcDigits(cnpj[:12]) {
 		return false
 	}
+
 	// Check second digit
 	if !calcDigits(cnpj[:13]) {
 		return false
 	}
+
 	return true
 }
 
+// atoi converts a string to an integer. Returns 0 on error.
 func atoi(s string) int {
 	i, _ := strconv.Atoi(s)
 	return i
 }
 
+// IsCPF determines if a document is a CPF based on its length.
 func IsCPF(document string) bool {
 	re := regexp.MustCompile(`\D`)
 	doc := re.ReplaceAllString(document, "")
 	return len(doc) == 11
 }
 
+// IsCNPJ determines if a document is a CNPJ based on its length.
 func IsCNPJ(document string) bool {
 	re := regexp.MustCompile(`\D`)
 	doc := re.ReplaceAllString(document, "")
